24151e1f746ec40062f6c6747174e93240ffafd9
Funciona Valentín <117476567+ZeloZalis@users.noreply.github.com>
2024-06-23 19:51:10 -0400

    Create index.py

---
+++ index.py
@@ -0,0 +1,25 @@
+# The way you import:
+import sys, os
+from datetime import date
+
+# Class and functions are writed like this:
+Class FileChecker():
+  # There is two ways for functions:
+  def pdf_check(self):
+    pass
+
+  def docCheck(self):
+    pass
+
+# For variables and constants
+one_variable = (x*7)
+CONSTANT = 'https://github.com/ZeloZalis'
+
+# This is the correct way to use whitespace:
+if x == 4: print(x, y); x, y = y, x
+spam(ham[1], {eggs: 2})
+i = i + 1
+submitted += 1
+x = x*2 - 1
+hypot2 = x*x + y*y
+c = (a+b) * (a-b)
